3494390eef711bbc0a3a9ea8ed76069db1e83cac
ack <acktic@gmail.com>
2019-12-05 13:13:07 -0800

    added windowscentral feed

--- rss.js
+++ rss.js
@@ -256,6 +256,7 @@ var menu=[
 {cat:"Technology",des:"Washington Post is a major American daily newspaper published in Washington, D.C.",uri:"http://feeds.washingtonpost.com/rss/business/technology",ext:"https://washingtonpost.com/business/technology"},
 {cat:"Technology",des:"Washington Times is an American daily newspaper published in Washington, D.C.",uri:"https://www.washingtontimes.com/rss/headlines/culture/technology",ext:"https://washingtontimes.com/culture/technology"},
 {cat:"Technology",des:"Wccftech has the Latest updates in the world of Tech, Mobile, PC Hardware, Gaming & Finance.",uri:"https://wccftech.com/feed",ext:"https://wccftech.com"},
+{cat:"Technology",des:"Windows Central News, Forums, Reviews, Help for Windows 10 and all things Microsoft.",uri:"https://www.windowscentral.com/rss",ext:"https://www.windowscentral.com"},
 {cat:"Technology",des:"WIRED is where tomorrow is realized. It is the essential source of information.",uri:"https://www.wired.com/feed/rss",ext:"https://wired.com"},
 {cat:"Technology",des:"Yahoo is a web services provider headquartered in California, and owned by Verizon Media.",uri:"https://www.yahoo.com/tech/rss",ext:"https://finance.yahoo.com/tech"},
 {cat:"Technology",des:"ZDNet is a business technology news website published by CBS Interactive.",uri:"https://www.zdnet.com/news/rss.xml",ext:"https://zdnet.com"},
