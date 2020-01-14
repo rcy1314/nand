64e34d7cc6afaae324d0a5d51368c4d3d05a2d07
ack <acktic@gmail.com>
2020-01-14 15:25:10 -0800

    added css subreddit

--- rss.js
+++ rss.js
@@ -338,6 +338,7 @@ var menu=[
 {cat:"Social",des:"Command Line subreddit shares tips, questions, and answers.",uri:"https://reddit.com/r/commandline/.rss",ext:"https://reddit.com/r/commandline"},
 {cat:"Social",des:"Computer subreddit covers computers, questions, tips, and suggestions.",uri:"https://reddit.com/r/computers/.rss",ext:"https://reddit.com/r/computers"},
 {cat:"Social",des:"Crypto subreddit covers the theory and practice of modern and *strong* cryptography, and it is a technical subreddit focused on the algorithms and implementations of cryptography.",uri:"https://reddit.com/r/crypto/rss",ext:"https://reddit.com/r/crypto"},
+{cat:"Social",des:"CSS subreddit For discussing Cascading Style Sheets, design principles, and technological innovations related to web development.",uri:"https://reddit.com/r/css/.rss",ext:"https://reddit.com/r/css"},
 {cat:"Social",des:"DataHoarder subreddit bringing data hoarders together to share their passion with like minded people.",uri:"https://reddit.com/r/DataHoarder/.rss",ext:"https://reddit.com/r/DataHoarder"},
 {cat:"Social",des:"DataIsBeautiful subreddit for visualizations that effectively convey information.",uri:"https://reddit.com/r/dataisbeautiful/.rss",ext:"https://reddit.com/r/dataisbeautiful"},
 {cat:"Social",des:"Debian subreddit has questions and topics revolving around Debian OS.",uri:"https://reddit.com/r/debian/.rss",ext:"https://reddit.com/r/debian"},
