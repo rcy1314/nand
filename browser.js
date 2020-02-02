c011bf2223ed0cdbf8e16f3cc32e442a5686e547
acktic <acktic@gmail.com>
2020-02-02 09:35:49 -0800

    hrm

--- browser.js
+++ browser.js
@@ -194,16 +194,11 @@ function ago(n) {
 
     var age = new Date()
     var dis = age.getTime() - new Date(n).getTime()
-    var sec = dis / 1000
-    if (sec < 60) return parseInt(sec) + ' second' + (parseInt(sec) > 1 ? 's' : '') + ' ago'
-    var min = sec / 60;
-    if (min < 60) return parseInt(min) + ' minute' + (parseInt(min) > 1 ? 's' : '') + ' ago'
-    var h = min / 60;
-    if (h < 24) return parseInt(h) + ' hour' + (parseInt(h) > 1 ? 's' : '') + ' ago'
-    var d = h / 24;
-    if (d < 30) return parseInt(d) + ' day' + (parseInt(d) > 1 ? 's' : '') + ' ago'
-    var m = d / 30;
-    if (m < 12) return parseInt(m) + ' month' + (parseInt(m) > 1 ? 's' : '') + ' ago'
+    var sec = dis / 1000; if (sec < 60) return parseInt(sec) + ' second' + (parseInt(sec) > 1 ? 's' : '') + ' ago'
+    var min = sec / 60; if (min < 60) return parseInt(min) + ' minute' + (parseInt(min) > 1 ? 's' : '') + ' ago'
+    var h = min / 60; if (h < 24) return parseInt(h) + ' hour' + (parseInt(h) > 1 ? 's' : '') + ' ago'
+    var d = h / 24; if (d < 30) return parseInt(d) + ' day' + (parseInt(d) > 1 ? 's' : '') + ' ago'
+    var m = d / 30; if (m < 12) return parseInt(m) + ' month' + (parseInt(m) > 1 ? 's' : '') + ' ago'
     var y = m / 121
 
     return parseInt(y) + ' year' + (parseInt(y) > 1 ? 's' : '') + ' ago'
