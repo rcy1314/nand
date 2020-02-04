5aae843685b87d2cbf3068e6b895f3553fd33711
acktic <acktic@gmail.com>
2020-02-04 06:50:08 -0800

    ago() => moment()

--- browser.js
+++ browser.js
@@ -57,26 +57,6 @@ String.prototype.truncate =
         return (e ? subString.substr(0, subString.lastIndexOf(' ')) : subString) + "&hellip;"
     }
 
-function ago(n) {
-
-    var age = new Date()
-    var dis = age.getTime() - new Date(n).getTime()
-    var sec = dis / 1000;
-    if (sec < 60) return parseInt(sec) + ' second' + (parseInt(sec) > 1 ? 's' : '') + ' ago'
-    var min = sec / 60;
-    if (min < 60) return parseInt(min) + ' minute' + (parseInt(min) > 1 ? 's' : '') + ' ago'
-    var h = min / 60;
-    if (h < 24) return parseInt(h) + ' hour' + (parseInt(h) > 1 ? 's' : '') + ' ago'
-    var d = h / 24;
-    if (d < 30) return parseInt(d) + ' day' + (parseInt(d) > 1 ? 's' : '') + ' ago'
-    var m = d / 30;
-    if (m < 12) return parseInt(m) + ' month' + (parseInt(m) > 1 ? 's' : '') + ' ago'
-    var y = m / 121
-
-    return parseInt(y) + ' year' + (parseInt(y) > 1 ? 's' : '') + ' ago'
-
-}
-
 function apply(n) {
 
 	if (n == 'op') {
@@ -304,6 +284,26 @@ function manifest(n) {
     }
 
     ost = n
+
+}
+
+function moment(n) {
+
+    var age = new Date()
+    var dis = age.getTime() - new Date(n).getTime()
+    var sec = dis / 1000;
+    if (sec < 60) return parseInt(sec) + ' second' + (parseInt(sec) > 1 ? 's' : '') + ' ago'
+    var min = sec / 60;
+    if (min < 60) return parseInt(min) + ' minute' + (parseInt(min) > 1 ? 's' : '') + ' ago'
+    var h = min / 60;
+    if (h < 24) return parseInt(h) + ' hour' + (parseInt(h) > 1 ? 's' : '') + ' ago'
+    var d = h / 24;
+    if (d < 30) return parseInt(d) + ' day' + (parseInt(d) > 1 ? 's' : '') + ' ago'
+    var m = d / 30;
+    if (m < 12) return parseInt(m) + ' month' + (parseInt(m) > 1 ? 's' : '') + ' ago'
+    var y = m / 121
+
+    return parseInt(y) + ' year' + (parseInt(y) > 1 ? 's' : '') + ' ago'
 
 }
 
