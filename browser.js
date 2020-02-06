0e4e5ef093e3a638aab51fc5acb4c80d11a54e93
acktic <acktic@gmail.com>
2020-02-06 15:41:39 -0800

    pub id about title

--- browser.js
+++ browser.js
@@ -235,7 +235,7 @@ function response(n) {
                         "<div class='yt'><iframe src='" + src + "'></iframe>" + views +
                         "<div class='ago views' style='right:0em'>Courtesy <a onclick='window.open(\"" + menu[n].ext + "\")'>" + menu[n].id + "</a></div></div>"
                 } else {
-                    html = "<div class='item' onclick='window.open(\"" + ref.trim() + "\")'><div id='pub'>" + $(this).find('title:first').text().trim().truncate(90, true) + "</div><div id='pub'><a ext='" + menu[i].ext + "'>" + id + "</a></div>" +
+                    html = "<div class='item' onclick='window.open(\"" + ref.trim() + "\")'><div id='pub'><a ext='" + menu[i].ext + "'>" + id + "</a></div><div id='pub'>" + $(this).find('title:first').text().trim().truncate(90, true) + "</div>" +
                         "<div id='ago'>" + dst[0] + "<br>" + dst[1] + "</div>" +
                         "<div class='ago attr'></div>" +
                         "<img onclick='event.stopPropagation(); expand(" + i + ")' id='" + i + "' style='display:none' src='" + src + "' class='img'>" + courtesy + '</div>'
