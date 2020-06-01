b51c8d4e1bfa5be9efb36a006e62fdcd7436d3db
acktic <acktic@gmail.com>
2020-06-01 09:11:32 -0700

    maintenance

--- browser.js
+++ browser.js
@@ -291,7 +291,7 @@ function imageResolution(n) {
             $('#' + n).siblings('.attr').html(Math.round($('#' + n).get(0).naturalWidth) + 'x' + Math
                 .round($('#' + n).get(0).naturalHeight) + '&ensp;' + expand)
         })
-    } else $('#' + n).parent().css('padding-bottom', '.5em')
+    } else $('#' + n).parent().css('padding-bottom', '1.5em')
 
 }
 
