bbd870598c6ac44c8ce512a221620a3fe1db39ca
acktic <acktic@gmail.com>
2020-06-11 20:29:48 -0700

    listing text bottom 5px

--- browser.js
+++ browser.js
@@ -598,7 +598,7 @@ function listResponse(n) {
 		menu.indexOf(menu[n]) + "' tabIndex='-1' response='" + 
 		n + "'><img class='type' src='" + img + "'>" + 
 		"<div class='text'>&emsp;" + menu[n].cat + 
-		"&ensp;" + menu[n].id.match(/[^\/]+$/g) + "</div>" +
+		"<br>&emsp;" + menu[n].id.match(/[^\/]+$/g) + "</div>" +
 		"</div>"
 	)
 	if ($('#search .listing .' + n).length > 1) $(
