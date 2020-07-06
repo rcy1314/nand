342dcb155cde2f62f01cd61f01963e92611abbd3
ack <acktic@gmail.com>
2020-07-06 16:30:21 -0700

    slice

--- js/init.js
+++ js/init.js
@@ -2,14 +2,8 @@ const myDecipher = decipher('mySecretSalt')
 
 if (location.href.split('?')[1])
 	if (location.href.split('?')[1].match(/^[a-z0-9\+1]+$/i)) {
-		if (location.href.match('\\+1'))
-
-			contrast = true
-
-	var strip = location.href.split('?')[1].replace(/\+1/g, '')
-	var re = strip.slice(0,6)
-	var id = myDecipher(re)
-	var ts = parseInt(strip.slice(6), '36')
+	var id = myDecipher(location.href.split('?')[1].slice(0, 6))
+	var ts = parseInt(location.href.split('?')[1].slice(6, 14), '36')
 	var loc = window.location.origin + '?q=&' + id + ts
 	if (contrast == true) loc = loc + '+1'
 	location.href = loc
