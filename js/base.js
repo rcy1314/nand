c540dcaec53ae3bc20e7cf095ee1f38e4eb54e3b
acktic <acktic@gmail.com>
2020-07-12 13:16:18 -0700

    visual opposite bg-color

--- js/base.js
+++ js/base.js
@@ -15,7 +15,7 @@ var visual = function(n) {
     $(
       '#container, #main, #arm, #home, #option, #bottom, .fa-home, .fa-code, .fa-terminal, .fa-git, .fa-globe, .fa-circle, .fa-circle-notch, input[type=text], #visit, .result, .air, .filter, .populate, .feed, .comment, .channel, .suggestions, .combine, .index, .title, .category, .description, .type, .item, .item .pub, .ago, a'
     ).css({
-      'background-color': '#000',
+      'background-color': '#212121',
       'color': '#fff',
       'border': 'none',
       'box-shadow': 'none'
