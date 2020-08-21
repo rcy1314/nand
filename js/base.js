e678b5da62d70d6b318ce9a9a9dbf0804dbebb5c
acktic <acktic@gmail.com>
2020-08-21 07:14:51 -0700

    index hover uri not ampersand, xml oldest pub.length

--- js/base.js
+++ js/base.js
@@ -691,7 +691,7 @@ var xml = function(e, s, n) {
 
     var posts = $('#main .center .channel .item').length
     var recent = pub[0].dst
-    var oldest = pub[$('.item:last').attr('item')].dst
+    var oldest = pub[pub.length - 1].dst
     if (e != 'search') $('#main .center').append(
       "<div id='bottom'>" +
       "  <button class='back' index='" + menu.indexOf(menu[n.toString().back()]) + "'>Previous</button>&ensp;" +
