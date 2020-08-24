1d913bc098a7f401304d8eb87003488ef329206b
acktic <acktic@gmail.com>
2020-08-23 20:21:46 -0700

    item hover bg color feedopposite radial def item margin bottom

--- js/visual.js
+++ js/visual.js
@@ -2,9 +2,9 @@ var visual = function(n) {
   if (n == 'op') op = op != true
   else if (n == 1 || n == 0) op = n
   if (op == 1) {
-    $('#container, #arm, #main, #main #option, #visit, #bottom, ' +
-      '.feed, .channel, .combine, .index, a, .stats, ' +
-      '.result, .air, #main .page, #main .page .id, .blur, ' +
+    $('#container, #arm, #main, #main #option, #visit, #bottom, .header, .classic, .wrap, .item, ' +
+      '.feed, .channel, .combine, .index, a, .stats, .comment, .ago, .pub, .tag' +
+      '.result, .air, #main .page, #main .page .id, .blur, input[type=text], ' +
       '.filter, .populate, .filter a, .populate a, .focus'
     ).css({
       'background-color': '#000',
@@ -12,24 +12,24 @@ var visual = function(n) {
       'border': 'none',
       'color': '#fff'
     })
-    $('#top, #arm, #arm #option, .wrap, ' +
-      '.header, .item, .comment, .ago, ' +
-      '.pub, .tag, .suggestions, .right, .left, #guide, ' +
-      '.item .pub, .more, .fas, .tag, #home a, .description, .type, ' +
-      '#home, input[type=text]'
+    $('#top, #arm, #arm #option, ' +
+      '.suggestions, .right, .left, #guide, ' +
+      '.fas, #home a, .description, .type, ' +
+      '#home'
     ).css({
       'background-color': '#0e0e0e',
       'box-shadow': 'none',
       'border': 'none',
       'color': '#fff'
     })
-    $('.buttonSearch, .type, .translation, .suggestions, .combine').css({
+    $('.buttonSearch, .type, .translation, .suggestions, .combine, .combine div').css({
       'background-color': 'transparent',
       'box-shadow': 'none',
       'color': '#fff'
     })
-    $('.detail .radial, .select .radial').removeClass('selectInvert').addClass('selectOpposite')
+    $('.item, .feed').css('border','.3px solid #000')
     $('.header .radial, .suggestions .radial').removeClass('suggestInvert').addClass('suggestOpposite')
+    $('.detail .radial, .select .radial').removeClass('selectInvert').addClass('selectOpposite')
     $('.feed .radial, .stats .radial').removeClass('feedInvert').addClass('feedOpposite')
     $('#progressBar').removeClass('responseInvert').addClass('responseOpposite')
     $('.focus').removeClass('pageinput pageInputOut').css('box-shadow','none')
@@ -47,17 +47,33 @@ var visual = function(n) {
     $('.category').css('border','1px solid #000')
     $('.hover').addClass('contrast.hover')
     $('#placeholder').css('color','#fff')
+    $('.item, .classic').on('mouseenter', function(e){
+      $(this)
+        .parents('.item').css({
+          'background-color': '#f6f8fa',
+          'border': '.3px solid #ddd'
+        })
+        .find('.classic, .img, .header, .wrap, .pub, .ago, .comment').css('background-color','#0e0e0e')
+    })
+    $('.item, .classic').on('mouseleave', function(e){
+      $(this)
+        .parents('.item').css({
+          'background-color': '#ffffff',
+          'border': '.3px solid #fff'
+        })
+        .find('.classic, .img, .header, .wrap, .pub, .ago, .comment').css('background-color','#000000')
+    })
   } else if (op == 0) {
-    $('#top, #arm, #container, .wrap, #arm, #arm #option, .index, .item, ' +
-      '.comment, .channel, #main .center .feed, .header, .page input[type=text], ' +
-      '.item, .item .pub, .type, .ago, .more, a'
+    $('#top, #arm, #container, #arm, #arm #option, #main, .channel, .index, .item, .classic, .wrap, ' +
+      '.comment, .channel, #main .center .feed, .page input[type=text], .tag, .pub, .ago, .comment, ' +
+      '.type, .stats, .combine, #bottom, a'
     ).css({
       'background-color': '#fff',
       'color': '#666',
       'border': 'none'
     })
-    $('#main, #container,.air, .result, #front #option, #visit, #bottom, .hover, .info a, ' +
-      '.channel, .stats, .combine, #bottom, #main .page .id, ' +
+    $('#container, .air, .result, #front #option, #visit, .hover, .info a, ' +
+      '#main .page .id, ' +
       '#main .page .feed, #main .page, .right, .left, #main .page .feed a, ' +
       '.filter, .populate, .blur, #search input[type=text], .focus'
     ).css({
@@ -65,13 +81,15 @@ var visual = function(n) {
       'border': 'none',
       'color': '#666'
     })
-    $('#home, .description, .tag, #option, .fas, .title, .type, .translation, ' +
-      '.tag, #option, .fa-double-angle-right, .fa-double-angle-left, #toggle, .suggestions'
+    $('#home, .description, .tag, #option, .fas, .title, .type, .translation, .wrap, .header, ' +
+      '.tag, #option, .fa-double-angle-right, .fa-double-angle-left, #toggle, .suggestions, .combine div, ' +
+      '.comment, .item .pub, .more, .ago'
     ).css({
       'background-color': 'transparent',
       'box-shadow': 'none',
       'color': '#000'
     })
+    $('.item, .feed').css('border','.3px solid #fff')
     $('.detail .radial, .select .radial').removeClass('selectOpposite').addClass('selectInvert')
     $('#guide .wrap, .page input, .item, .right, .left, .page .id').css('box-shadow', '1px 1px 1px #eee')
     $('#search input[type=text], .filter, .populate, .center .feed').css('border', '1px solid #eaeaea')
@@ -90,5 +108,21 @@ var visual = function(n) {
     $('#handle a, #placeholder').css('color', '#666')
     $('#favicon').attr('href', 'favicon.ico')
     $('.hover').addClass('visual.hover')
+    $('.item, .classic').on('mouseenter', function(e){
+      $(this)
+        .parents('.item').css({
+          'background-color': '#f6f8fa',
+          'border': '.3px solid #ddd'
+        })
+        .find('.classic, .img, .header, .wrap, .pub, .ago, .comment').css('background-color','#f6f8fa')
+    })
+    $('.item, .classic').on('mouseleave', function(e){
+      $(this)
+        .parents('.item').css({
+          'background-color': '#ffffff',
+          'border': '.3px solid #fff'
+        })
+        .find('.classic, .img, .header, .wrap, .pub, .ago, .comment').css('background-color','#ffffff')
+    })
   }
 }
