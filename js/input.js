e47499296bcc223cc2bc6b4d306c119824bea30d
acktic <acktic@gmail.com>
2020-08-25 10:28:15 -0700

    page input touch click if search class removal

--- js/input.js
+++ js/input.js
@@ -95,21 +95,11 @@ $(document)
       )
     })
     $(this).val('')
-    if ($('#main #visit .page #front .icon').hasClass('search'))
       $(this).css({
             'caret-color': '#e4e4e4',
             'padding-left': '40px',
             'text-align': 'left'
       })
-      else if (!$('#main #visit .page #front .icon').hasClass('search')) {
-        setTimeout(function() {
-          $this.css({
-            'caret-color': '#e4e4e4',
-            'padding-left': '40px',
-            'text-align': 'left',
-          })
-        }, 200)
-      }
       $('#main #visit .page #front .icon').addClass('search')
     visual()
 })
