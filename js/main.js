7d80e3c6506bb91e25e0ddb3097d52e95aaed4aa
acktic <acktic@gmail.com>
2020-08-25 21:38:12 -0700

    mislabeled uri

--- js/main.js
+++ js/main.js
@@ -26,8 +26,8 @@ $(document)
   })
   .on('touch click', '#arm #home', function(e) {
     var uri = window.location.origin
-    if (contrast == true && !location.href.match('\\+1')) n = n + '?+1'
-    else if (contrast == true) n = n + '?+1'
+    if (contrast == true && !location.href.match('\\+1')) uri = uri + '?+1'
+    else if (contrast == true) uri = uri + '?+1'
     uri.exit()
     e.preventDefault()
   })
