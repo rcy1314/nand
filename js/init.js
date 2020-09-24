3bd1776e4e54afaaddcc4d38aa619b9487322f3c
acktic <acktic@gmail.com>
2020-09-24 07:46:04 -0700

    init query loading

--- js/init.js
+++ js/init.js
@@ -72,17 +72,9 @@ if (location.search.split('?q=')[1]) {
   $(document)
     .ready(function() {
 
+        $.loading()
         $('html body #wrapper #container #main #top').show()
         var width = $('html body #wrapper #container #main').width() / 30
-        complete = setInterval(function() {
-          $('#progressBar').css({
-            '-webkit-transition-delay': '0s',
-            '-webkit-transition': '.95s',
-            '-moz-transition-delay': '0s',
-            '-moz-transition': '.95s'
-          }).width($('#progressBar').width() + Math.floor(Math.random() * (100 - width) + width))
-        }, 750)
-
         if (!uri[1] && location.href.match('\\&')) response(true, false, uri[0], false)
         else if (!uri[1]) response(true, false, uri[0], true)
         else if (uri[1]) response(true, uri[0], uri[1], false)
