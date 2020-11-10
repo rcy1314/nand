ebda07c7a6e3f1b7a3f541b301960e5b0fe0cf83
ack <acktic@gmail.com>
2020-11-09 18:21:24 -0800

    reflect off ticking

--- maintenance/rip.sh
+++ maintenance/rip.sh
@@ -2,4 +2,4 @@
 #curl -q -s "https://acktic.github.io/js/xmlAssets.js" | grep uri | awk '{print $2}' | tr -d ',' | tr -d '"' | grep http | xargs -n1 -P 10 -I {} bash -c 'curl --connect-timeout 10 -q -s -q -k "{}" -I | grep -e rss -e text -e xml -e html -e json >/dev/null && echo "Feed is live: {}" || echo "Feed is dead: {}"' | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }'
 
 #Minified
-curl -q -s https://acktic.github.io/js/xmlAssets.js | grep 'uri:.*` | sed '/suri://g' | cut -f1 -d, | tr -d "`" | grep http | xargs -n1 -P 10 -I {} bash -c 'curl --connect-timeout 10 -q -s -q -k "{}" -I | grep -e rss -e text -e xml -e html -e json >/dev/null && echo "Feed is live: {}" || echo "Feed is dead: {}"' | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }'
+curl -q -s https://acktic.github.io/js/xmlAssets.js | grep -oP 'uri:`.*' | sed 's/uri://g' | cut -f1 -d, | tr -d '`' | grep http | xargs -n1 -P 10 -I {} bash -c 'curl --connect-timeout 10 -q -s -q -k "{}" -I | grep -e rss -e text -e xml -e html -e json >/dev/null && echo "Feed is live: {}" || echo "Feed is dead: {}"' | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }'
