83a6c869bd97c3a1f7efd8520ba3511b8647a4fc
acktic <acktic@gmail.com>
2020-11-17 08:42:52 -0800

    move themes loading to init.js

--- js/init.js
+++ js/init.js
@@ -50,7 +50,34 @@
 
 setTimeout(function() {
 
-  _container.style.display = `block`;
+    _container.style.display = `block`;
+
+    let directory = `js/themes/`;
+    let extension = `.js`;
+
+    for (var visual of visuals) {
+      let path = directory + visual + extension;
+      let script = document.createElement(`script`);
+      script.type = `text/javascript`;
+      script.src = path;
+      document.getElementsByTagName(`head`)[0].appendChild(script);
+    }
+
+    for (i = 0; i <= themes.length - 1; i++) {
+      (function (i) {
+        var theme = themes[i].name;
+        document.addEventListener(
+          `click`,
+          function (event) {
+            if (event.target.classList.contains(theme)) {
+              initial = theme;
+              window[theme]();
+            }
+          },
+          false
+        );
+      })(i);
+    }
 
     var startup = setInterval(function () {
       if (typeof menu === `array`) {
--- js/mainEvents.js
+++ js/mainEvents.js
@@ -1,30 +1,4 @@
 window.onload = function () {
-  let directory = `js/themes/`;
-  let extension = `.js`;
-
-  for (var visual of visuals) {
-    let path = directory + visual + extension;
-    let script = document.createElement(`script`);
-    script.type = `text/javascript`;
-    script.src = path;
-    document.getElementsByTagName(`head`)[0].appendChild(script);
-  }
-
-  for (i = 0; i <= themes.length - 1; i++) {
-    (function (i) {
-      var theme = themes[i].name;
-      document.addEventListener(
-        `click`,
-        function (event) {
-          if (event.target.classList.contains(theme)) {
-            initial = theme;
-            window[theme]();
-          }
-        },
-        false
-      );
-    })(i);
-  }
   _guest.setAttribute(`placeholder`, `Search Feeds`);
   _guest.style.caretColor = `#e4e4e4`;
   _guest.style.paddingLeft = `40px`;
