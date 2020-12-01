f37333414657d7600030f3af0ebb9b1747999e9c
acktic <acktic@gmail.com>
2020-12-01 06:35:45 -0800

    move guideDisplay to xmlFunctions.js

--- js/baseFunctions.js
+++ js/baseFunctions.js
@@ -233,22 +233,6 @@ var quickFeedDisplay = function (Value) {
   }
 };
 
-var guideDisplay = function (pubArray) {
-  _guide.style.display = `flex`;
-  _guide.append(guideBuild(pubArray[0]));
-  document.querySelector(`.sticky`).style.display = `none`
-  guideImageAttributes(pubArray[0]);
-};
-
-var guideDisplayYoutube = function (pubArray) {
-  _guide.innerHTML = `
-  <svg class='checkmark' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 52 52'>
-    <circle class='checkmark__circle' cx='26' cy='26' r='25' fill='none' />
-    <path class='checkmark__check' fill='none' d='M16 16 36 36 M36 16 16 36' />
-  `;
-  _guide.append(guideBuildYoutube(pubArray[0]));
-};
-
 var quickFeedAsset = function (feedAssets) {
   let duplicate = [];
   if (feedAssets == 7)
