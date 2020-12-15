d91e315902294e27e33c8dad7eb66234cc3ceafc
acktic <acktic@gmail.com>
2020-12-15 13:35:08 -0800

    filterInputResponse increase description length for one exact match

--- js/baseFunctions.js
+++ js/baseFunctions.js
@@ -306,7 +306,7 @@ let filterInputResponse = function (filterURI) {
     return item.description.space().toLowerCase()
       .match(filterURI.toString().toLowerCase().space());
   })
-  if (description.length > 0 && exact === -1) {
+  if (description.length > 1 && exact === -1) {
     groupBuild();
     for (i = 0; i <= description.length - 1; i++)
       writeFilterResponse(menu.indexOf(description[i]));
