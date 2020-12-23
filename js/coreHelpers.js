bb2b86e1c7e4d71682d699c3bd8904d02db0f1b5
acktic <acktic@gmail.com>
2020-12-23 06:26:04 -0800

    coreHelpers.js remove init count push

--- js/coreHelpers.js
+++ js/coreHelpers.js
@@ -13,7 +13,6 @@ var init = function () {
       .querySelectorAll(`#dots .fill`)
       .forEach((a) => (a.style.visibility = `visible`));
   } else if (loading == `percent`) {
-    count.push(`null`);
     progressBackDrop(false);
   }
 };
