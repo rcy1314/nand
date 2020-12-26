7c8075ac63c379403d15647362d3307c5a386946
acktic <acktic@gmail.com>
2020-12-26 13:11:29 -0800

    testing inline

--- js/themes/Alpenglow.js
+++ js/themes/Alpenglow.js
@@ -10,7 +10,7 @@ var Alpenglow = function () {
   document.documentElement.style.setProperty(`--fill-color`, `#ffffff`);
   document.documentElement.style.setProperty(
     `--color-primary`,
-    `rgba(255,255,255, .1)`
+    `rgba(255, 255, 255, .1)`
   );
   document.documentElement.style.setProperty(
     `--color-secondary`,
