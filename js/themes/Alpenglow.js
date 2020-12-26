0cd88c1d1eece71c063b1f93ba163c5223df9c50
acktic <acktic@gmail.com>
2020-12-26 12:09:33 -0800

    testing image in theme

--- js/themes/Alpenglow.js
+++ js/themes/Alpenglow.js
@@ -29,8 +29,8 @@ var Alpenglow = function () {
     `rgba(63, 92, 136, .4)`
   );
   document.querySelector(`#favicon`).setAttribute(`href`, `favicon.ico`);
-  if (backgroundImage[0].path != `images/ffe869c642be33cbb2c3e609e27beb29.webp`)
-    backgroundImage[0].path = `images/ffe869c642be33cbb2c3e609e27beb29.webp`
+  if (backgroundImage[0].path != `images/b453ae624e3d5e58b9890a998ec441b8.webp`)
+    backgroundImage[0].path = `images/b453ae624e3d5e58b9890a998ec441b8.webp`
   document.querySelector(`#${backgroundImage[0].element}`)
     .style.backgroundImage = `url(${backgroundImage[0].path})`
 };
