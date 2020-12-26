85c364a703153ece6e92404111dc104cdd6f6eab
acktic <acktic@gmail.com>
2020-12-26 12:49:17 -0800

    replace wallpapers

--- js/themes/Night.js
+++ js/themes/Night.js
@@ -31,8 +31,8 @@ var Night = function () {
   document
     .querySelector(`#favicon`)
     .setAttribute(`href`, `images/Opposite.ico`);
-  if (backgroundImage[0].path != `https://raw.githubusercontent.com/acktic/acktic.github.io/master/images/a54430a6cf0248fa8d1d5961e02a71e4.webp`)
-    backgroundImage[0].path = `https://raw.githubusercontent.com/acktic/acktic.github.io/master/images/a54430a6cf0248fa8d1d5961e02a71e4.webp`
+  if (backgroundImage[0].path != `images/a54430a6cf0248fa8d1d5961e02a71e4.webp`)
+    backgroundImage[0].path = `images/a54430a6cf0248fa8d1d5961e02a71e4.webp`
   document.querySelector(`#${backgroundImage[0].element}`)
     .style.backgroundImage = `url(${backgroundImage[0].path})`
 };
