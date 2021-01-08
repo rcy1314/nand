fda3f6b0870e1caa600f160bd643771a9c46af61
acktic <acktic@gmail.com>
2021-01-08 12:15:36 -0800

    edit favorites

--- js/settings.js
+++ js/settings.js
@@ -41,13 +41,10 @@ const favorites = [
   `Reddit/Pics`,
   `Reddit/Wallpaper`,
   `Reddit/Wallpapers`,
-  `Reddit/Unix Porn`,
-  `Reddit/City Porn`,
-  `Reddit/Earth Porn`,
   `Reddit/Los Angeles`,
   `Reddit/San Francisco`,
-  `4Chan/Technology`,
   `4Chan/Wallpapers`,
+  `4Chan/Technology`,
 ]
 
 /*
