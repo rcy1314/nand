d5aa5e95c03e469c5108bd1adf20dc6317c69664
acktic <acktic@gmail.com>
2021-06-17 12:43:42 -0700

    heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static('/'))
+app.use('/', express.static('/home/ack/acktic.github.io'))
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
 	console.log('Your files will be served through this web server')
