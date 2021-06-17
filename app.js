de025ad5300500d562395cdff7c973a21bca16f2
acktic <acktic@gmail.com>
2021-06-17 12:59:54 -0700

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static(''))
+app.use('/', express.static('/'))
 var port = Number(process.env.PORT || 3000);
 app.listen(port, function() {
 	console.log('Your files will be served through this web server')
