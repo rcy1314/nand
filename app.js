8608b928a7677a4c19ab2c9d44746615a44fd7e8
acktic <acktic@gmail.com>
2021-06-17 19:42:39 -0700

    reflect site dir

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static(__dirname + '/site'));
+app.use('/', express.static(__dirname));
 var port = Number(process.env.PORT || 3000);
 app.listen(port, function() {
 	console.log('Your files will be served through locahost:3000 use your preferred browser')
