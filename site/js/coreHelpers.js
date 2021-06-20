84a9cd37597beb3ea43c6c0f004684277bfe116c
ack <acktic@gmail.com>
2021-06-20 11:03:53 -0700

    remove category restriction from justRead

--- site/js/coreHelpers.js
+++ site/js/coreHelpers.js
@@ -71,7 +71,6 @@ var anyRandomMenuObject = function () {
     ) {
       if (
         menu[i] &&
-        menu[i].category == category &&
         menu[i].media == true
       )
         random.push(menu.indexOf(menu[i]));
