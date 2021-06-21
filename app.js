db44c68c9a4a99e9bd37c0e62dab5551415330a0
acktic <acktic@gmail.com>
2021-06-21 13:07:15 -0700

    node opener test

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var opn = require('open');
 var app = express();
 app.use('/', express.static(__dirname));
 var port = Number(process.env.PORT || 3000);
 app.listen(port, function() {
 	console.log('Your files will be served through locahost:3000 use your preferred browser')
 });
+opn('http://localhost:3000/index.html')
