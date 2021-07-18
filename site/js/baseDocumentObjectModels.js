bf31ae27ecf1abab097fe79cdc31ac90b652de37
Joey Banks <acktic@gmail.com>
2021-07-18 07:24:06 -0700

    baseDocumentObjectModels.js displayFlex Math.min column bottom order num

--- site/js/baseDocumentObjectModels.js
+++ site/js/baseDocumentObjectModels.js
@@ -76,34 +76,43 @@ let displayLegacy = function () {
 }
 
 let displayFlex = function () {
-    var height = 0;
-    var second = 0;
-    var groups = 0;
-    var column = _channel.querySelectorAll(`.item:nth-child(3n+1)`);
-    for (i = 0; i < column.length - 1; i++) height += column[i].clientHeight;
-    var column = _channel.querySelectorAll(`.item:nth-child(3n+2)`);
-    for (i = 0; i < column.length - 1; i++) second += column[i].clientHeight;
-    var column = _channel.querySelectorAll(`.item:nth-child(3n+3)`);
-    for (i = 0; i < column.length - 1; i++) groups += column[i].clientHeight;
-    var max = Math.max(height, second, groups);
-    _center.classList.remove(`sideChannel`);
-    _center.style.cssText = `display:inline-flex;width:930px;left:320px`;
-    _channel.style.height = `${(max + 1000).toString()}px`
-    _channel.classList.remove(`sideChannel`);
-    _channel.classList.add(`flexbox`);
-    _channel
-      .querySelectorAll(`.item`)
-      .forEach(
-        (a) => a.classList.add(`flexbox`)
-      );
-    _channel
-      .querySelectorAll(`.item`)
-      .forEach(
-        (a) => a.style.marginLeft = `0`
-      );
-    if (_main.clientWidth < 1280) {
-      _display.style.display = `none`;
-    }
+  setTimeout(
+    function () {
+      var height = 0;
+      var second = 0;
+      var groups = 0;
+      var column = _channel.querySelectorAll(`.item:nth-child(3n+1)`);
+      for (i = 0; i < column.length - 1; i++) height += column[i].clientHeight;
+      var column = _channel.querySelectorAll(`.item:nth-child(3n+2)`);
+      for (i = 0; i < column.length - 1; i++) second += column[i].clientHeight;
+      var column = _channel.querySelectorAll(`.item:nth-child(3n+3)`);
+      for (i = 0; i < column.length - 1; i++) groups += column[i].clientHeight;
+      var max = Math.max(height, second, groups);
+      var min = Math.min(height, second, groups);
+      if (height == min) var min = 0;
+      else if (second == min) var min = 1;
+      else if (groups == min) var min = 2;
+      _channel.querySelector(`#bottom`).style.order = min;
+      _channel.style.height = `${(max + 1000).toString()}px`
+    }, 200
+  )
+  _center.classList.remove(`sideChannel`);
+  _center.style.cssText = `display:inline-flex;width:930px;left:320px`;
+  _channel.classList.remove(`sideChannel`);
+  _channel.classList.add(`flexbox`);
+  _channel
+    .querySelectorAll(`.item`)
+    .forEach(
+      (a) => a.classList.add(`flexbox`)
+    );
+  _channel
+    .querySelectorAll(`.item`)
+    .forEach(
+      (a) => a.style.marginLeft = `0`
+    );
+  if (_main.clientWidth < 1280) {
+    _display.style.display = `none`;
+  }
 }
 
 let displaySideScroll = function () {
