ceeee0596c616c13ddc11729b31fe52fcbd41043
Joey Banks <acktic@gmail.com>
2021-11-06 09:08:00 -0700

    Comment main/Assets.js

--- site/js/main/sidebar/Background/DoubleClick.js
+++ site/js/main/sidebar/Background/DoubleClick.js
@@ -1,4 +1,4 @@
-object
+_sidebar
   .addEventListener(
     'dblclick', (evt) => {
       if (
@@ -7,71 +7,74 @@ object
         .classList
         .contains(
           `urlInput`
+        )
+      ) {
 
-        ) {
-
-          navigator.clipboard.readText()
-            .then((result) => {
-              onScreen = true;
-              sideBarLock = true;
-              object.value = result;
-              if (
-                _sidebar
-                .querySelector(
-                  `.imageURL`
-                )
-                .value
-                .match(
-                  /\b(?:png|jpe?g|gif|webp)/g
-                )
-              ) {
-                if (
-                  backgroundImage[0]
-                  .element ==
-                  `container`
-                ) {
-                  _container
-                    .style
-                    .backgroundImage =
-                    `url(
-                      ${
-                      _sidebar.querySelector(`.imageURL`).value
-                      }
-                    )`;
-                  _main
-                    .style
-                    .backgroundImage =
-                    `url()`;
-                } else if (
-                  backgroundImage[0]
-                  .element ==
-                  `main`
-                ) {
-                  _main
-                    .style
-                    .backgroundImage =
-                    `url(
-                      ${
-                      _sidebar.querySelector(`.imageURL`).value
-                      }
-                    )`;
-                  _container
-                    .style
-                    .backgroundImage =
-                    `url()`;
-                }
-              }
-              setTimeout(
-                function() {
-                  sideBarLock = sideBarLockBuffer;
-                }, 7000
+        navigator.clipboard.readText()
+          .then((result) => {
+            onScreen = true;
+            sideBarLock = true;
+            _sidebar
+              .querySelector(
+                `.urlInput`
+              ).value = result;
+            if (
+              _sidebar
+              .querySelector(
+                `.imageURL`
               )
-            })
-            .catch((error) => {
-              //console.log(error);
-            });
-        }
-      }, {
-        passive: false
+              .value
+              .match(
+                /\b(?:png|jpe?g|gif|webp)/g
+              )
+            ) {
+              if (
+                backgroundImage[0]
+                .element ==
+                `container`
+              ) {
+                _container
+                  .style
+                  .backgroundImage =
+                  `url(
+                      ${
+                      _sidebar.querySelector(`.imageURL`).value
+                      }
+                    )`;
+                _main
+                  .style
+                  .backgroundImage =
+                  `url()`;
+              } else if (
+                backgroundImage[0]
+                .element ==
+                `main`
+              ) {
+                _main
+                  .style
+                  .backgroundImage =
+                  `url(
+                      ${
+                      _sidebar.querySelector(`.imageURL`).value
+                      }
+                    )`;
+                _container
+                  .style
+                  .backgroundImage =
+                  `url()`;
+              }
+            }
+            setTimeout(
+              function() {
+                sideBarLock = sideBarLockBuffer;
+              }, 7000
+            )
+          })
+          .catch((error) => {
+            //console.log(error);
+          });
       }
-    );
+    }, {
+      passive: false
+    }
+  );
