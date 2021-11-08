d6a9e90397e4988945f2d143e7ad7e4d286c5c93
Joey Banks <acktic@gmail.com>
2021-11-08 15:27:30 -0800

    image/Attributes.js cleanup and onerror set attribute `Copy URL`

--- site/js/main/image/Attributes.js
+++ site/js/main/image/Attributes.js
@@ -84,11 +84,15 @@ var Attributes = function(empty, menuObject, pubIndex, src) {
     newImg = new Image();
     newImg.setAttribute(`src`, src);
     newImg.onerror = function() {
+      itemPending.remove();
       if (
         onlyImages
       )
         item.remove();
-      itemPending.remove();
+        copyDownload.style.display = `none`;
+        copyPicture.style.display = `none`;
+        copyPost.style.display = `none`;
+        attribute.style.height = `35px`;
     };
     newImg.onload = function() {
       let Height;
@@ -131,69 +135,54 @@ var Attributes = function(empty, menuObject, pubIndex, src) {
                       Height < shrunk
                     ) {
                       itemContainer.style.backgroundImage = `url(${e.target.result})`;
-                      copyDownload.style.display = `none`;
-                      copyPicture.style.display = `none`;
-                      copyPost.style.display = `block`;
-                      attribute.style.height = `74px`;
                       itemPending.remove();
-                  } else {
-                    Dimensions(
-                      menuObject,
-                      pubIndex,
-                      Height,
-                      Width
-                    )
-                    itemImage.setAttribute(`src`, e.target.result);
-                    itemFilter.style.transform = `scale(4)`
-                    itemPending.style.display = `none`;
-                    itemImage.style.display = `block`;
-                    itemFilter.classList.add(`blur`);
-                    setTimeout(
-                      function() {
-                        if (
-                          !cropImages
-                        ) {
-                          item.style
-                            .cssText =
-                            `height:${itemImage.clientHeight}px !important`
-                          itemWrap.style
-                            .cssText =
-                            `height:${itemImage.clientHeight}px !important`
-                          itemClassic.style
-                            .cssText =
-                            `height:${itemImage.clientHeight}px !important`
-                        }
-                      }, 1
-                    )
-                  };
+                    } else {
+                      Dimensions(
+                        menuObject,
+                        pubIndex,
+                        Height,
+                        Width
+                      )
+                      itemImage.setAttribute(`src`, e.target.result);
+                      itemFilter.style.transform = `scale(4)`
+                      itemPending.style.display = `none`;
+                      itemImage.style.display = `block`;
+                      itemFilter.classList.add(`blur`);
+                      setTimeout(
+                        function() {
+                          if (
+                            !cropImages
+                          ) {
+                            item.style
+                              .cssText =
+                              `height:${itemImage.clientHeight}px !important`
+                            itemWrap.style
+                              .cssText =
+                              `height:${itemImage.clientHeight}px !important`
+                            itemClassic.style
+                              .cssText =
+                              `height:${itemImage.clientHeight}px !important`
+                          }
+                        }, 1
+                      )
+                    };
+                  }
                 }
-              }
                 request.onerror = function(e) {
+                  itemPending.remove();
                   if (
                     onlyImages
                   )
                     item.remove();
-                  itemPending.remove();
+                    copyDownload.style.display = `none`;
+                    copyPicture.style.display = `none`;
+                    copyPost.style.display = `none`;
+                    attribute.style.height = `35px`;
                 };
                 if (
                   !src.match(/4cdn/g)
                 )
                   request.send();
-                else {
-                  Dimensions(
-                    menuObject,
-                    pubIndex,
-                    Height,
-                    Width
-                  )
-                  itemFilter.style.transform = `scale(4)`
-                  copyDownload.classList.add(`picture`);
-                  copyPicture.classList.add(`download`);
-                  itemImage.setAttribute(`src`, src);
-                  itemPending.style.display = `none`;
-                  itemImage.style.display = `block`;
-                  itemFilter.classList.add(`blur`);
-                }
               } else if (
                 jsonResponse.score <= safeSearchScore &&
                 document.body.contains(
@@ -216,75 +205,68 @@ var Attributes = function(empty, menuObject, pubIndex, src) {
                       Height < shrunk
                     ) {
                       itemContainer.style.backgroundImage = `url(${e.target.result})`;
-                      copyDownload.style.display = `none`;
-                      copyPicture.style.display = `none`;
-                      copyPost.style.display = `block`;
-                      attribute.style.height = `74px`;
                       itemPending.remove();
-                  } else {
-                    Dimensions(
-                      menuObject,
-                      pubIndex,
-                      Height,
-                      Width
-                    )
-                    itemImage.setAttribute(`src`, e.target.result);
-                    itemPending.style.display = `none`;
-                    itemImage.style.display = `block`;
-                    setTimeout(
-                      function() {
-                        if (
-                          !cropImages
-                        ) {
+                    } else {
+                      itemPending.remove();
+                      Dimensions(
+                        menuObject,
+                        pubIndex,
+                        Height,
+                        Width
+                      )
+                      itemImage.setAttribute(`src`, e.target.result);
+                      itemImage.style.display = `block`;
+                      setTimeout(
+                        function() {
+                          if (
+                            !cropImages
+                          ) {
 
-                          item.style
-                            .cssText =
-                            `height:${itemImage.clientHeight}px !important`
-                          itemWrap.style
-                            .cssText =
-                            `height:${itemImage.clientHeight}px !important`
-                          itemClassic.style
-                            .cssText =
-                            `height:${itemImage.clientHeight}px !important`
-                        }
-                      }, 1
-                    )
-                  };
+                            item.style
+                              .cssText =
+                              `height:${itemImage.clientHeight}px !important`
+                            itemWrap.style
+                              .cssText =
+                              `height:${itemImage.clientHeight}px !important`
+                            itemClassic.style
+                              .cssText =
+                              `height:${itemImage.clientHeight}px !important`
+                          }
+                        }, 1
+                      )
+                    };
+                  }
                 }
-              }
                 request.onerror = function(e) {
+                  itemPending.remove();
                   if (
                     onlyImages
                   )
                     item.remove();
-                  itemPending.remove();
+                    copyDownload.style.display = `none`;
+                    copyPicture.style.display = `none`;
+                    copyPost.style.display = `none`;
+                    attribute.style.height = `35px`;
                 }
                 if (
                   !src.match(/4cdn/g)
                 )
                   request.send();
-                else {
-                  Dimensions(
-                    menuObject,
-                    pubIndex,
-                    Height,
-                    Width
-                  )
-                  itemImage.setAttribute(`src`, src);
-                  itemPending.style.display = `none`;
-                  itemImage.style.display = `block`;
-                }
               }
             });
           })
           .catch((response) => {
             itemPending.remove();
+            if (
+              onlyImages
+            )
+              item.remove();
           });
       } else if (
-        document.body.contains(
-          _channel.querySelector(
-            `[aria-object='${menuObject}'][aria-item='${pubIndex}'] .image`
-          )
+        document
+        .body
+        .contains(
+          itemImage
         )
       ) { //safeSearch false
         var request = new XMLHttpRequest();
@@ -300,82 +282,72 @@ var Attributes = function(empty, menuObject, pubIndex, src) {
               menu[id].id.match(/Youtube/g)
             )
               itemImage.classList.add(`youtube`)
-              if (
-                Width < maximum ||
-                Height < shrunk
-              ) {
-                itemContainer.style.backgroundImage = `url(${e.target.result})`;
-                copyDownload.style.display = `none`;
-                copyPicture.style.display = `none`;
-                copyPost.style.display = `block`;
-                attribute.style.height = `74px`;
-                itemPending.remove();
+            if (
+              Width < maximum ||
+              Height < shrunk
+            ) {
+              itemContainer.style.backgroundImage = `url(${e.target.result})`;
+              itemPending.remove();
             } else {
-            Dimensions(
-              menuObject,
-              pubIndex,
-              Height,
-              Width
-            );
-            itemImage.setAttribute(`src`, e.target.result);
-            itemImage.style.display = `block`;
-            itemPending.remove();
-            setTimeout(
-              function() {
-                if (
-                  !cropImages
-                ) {
-
-                  item.style
-                    .cssText =
-                    `height:${itemImage.clientHeight}px !important`
-                  itemWrap.style
-                    .cssText =
-                    `height:${itemImage.clientHeight}px !important`
-                  itemClassic.style
-                    .cssText =
-                    `height:${itemImage.clientHeight}px !important`
-                }
-              }, 1
-            )
+              Dimensions(
+                menuObject,
+                pubIndex,
+                Height,
+                Width
+              );
+              itemImage.setAttribute(`src`, e.target.result);
+              itemImage.style.display = `block`;
+              itemPending.remove();
+              setTimeout(
+                function() {
+                  if (
+                    !cropImages
+                  ) {
+                    item.style
+                      .cssText =
+                      `height:${itemImage.clientHeight}px !important`
+                    itemWrap.style
+                      .cssText =
+                      `height:${itemImage.clientHeight}px !important`
+                    itemClassic.style
+                      .cssText =
+                      `height:${itemImage.clientHeight}px !important`
+                  }
+                }, 1
+              )
+            }
           }
         }
-      }
         request.onerror = function(e) {
+          itemPending.remove();
           if (
             onlyImages
           )
             item.remove();
-          itemPending.remove();
+            copyDownload.style.display = `none`;
+            copyPicture.style.display = `none`;
+            copyPost.style.display = `none`;
+            attribute.style.height = `35px`;
         };
         if (
           !src.match(/4cdn/g)
         )
           request.send();
-        else {
-          Dimensions(
-            menuObject,
-            pubIndex,
-            Height,
-            Width
-          )
-          itemImage.setAttribute(`src`, src);
-          itemPending.style.display = `none`;
-          itemImage.style.display = `block`;
-        }
       };
     };
     newImg.onerror = function() {
+      itemPending.remove();
       if (
         onlyImages
       )
         item.remove();
       copyDownload.style.display = `none`;
       copyPicture.style.display = `none`;
-      attribute.style.height = `74px`;
-      itemPending.remove();
+      copyPost.style.display = `none`;
+      attribute.style.height = `35px`;
     };
   } else {
+    itemPending.remove();
     if (
       onlyImages
     )
@@ -384,6 +356,5 @@ var Attributes = function(empty, menuObject, pubIndex, src) {
     copyPicture.style.display = `none`;
     copyPost.style.display = `none`;
     attribute.style.height = `35px`;
-    itemPending.remove();
   };
 }
