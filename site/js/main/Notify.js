8587c5b94fbad57095977b898584ea7267f43352
Joey Banks <acktic@gmail.com>
2021-11-12 09:14:57 -0800

    queued notify transitions dynamic

--- site/js/main/Notify.js
+++ site/js/main/Notify.js
@@ -1,32 +1,65 @@
 let notifyOption = function(String, Icon) {
-  _notify.innerHTML =
+  let div = document.createElement(`div`);
+  div.setAttribute(`id`, 'notify');
+  div.innerHTML =
     `
     ${String} <div class='notifyIcon fa ${Icon}'></div>
     `;
-  _notify.classList.add(`notify`);
-  _notify.style.display = `flex`;
+  div.style.display = `flex`;
+  div.classList.add(`notify`);
+  if (
+    document
+    .body
+    .contains(
+      _container.querySelector(`#notify`)
+    )
+  ) {
+    _container.querySelectorAll(`#notify`).forEach((a) => a.classList.add(`notifyEnd`))
+    _container.querySelectorAll(`#notify`)[0].classList.remove(`notify`);
+
+  }
+
+
+  _container.appendChild(div);
+
+  var transitionEvent = whichTransitionEvent();
+  transitionEvent && div.addEventListener(transitionEvent, function() {
+    console.log(`transition`);
+    if (
+      document
+      .body
+      .contains(
+        _container.querySelector(`.notifyEnd`)
+      )
+    )
+      _container.querySelectorAll(`.notifyEnd`).forEach(
+        (a) => a.remove()
+      )
+  });
 }
 
-function whichTransitionEvent(){
-    var t;
-    var transitions = {
-      'animation':'animationend',
-      'transition':'transitionend',
-      'OTransition':'oTransitionEnd',
-      'MozTransition':'transitionend',
-      'WebkitTransition':'webkitTransitionEnd'
-    }
+function whichTransitionEvent() {
+  var t;
+  var transitions = {
+    'animation': 'animationend',
+    'transition': 'transitionend',
+    'OTransition': 'oTransitionEnd',
+    'MozTransition': 'transitionend',
+    'WebkitTransition': 'webkitTransitionEnd'
+  }
 
-    for(t in transitions){
-        if( _notify.style[t] !== undefined ){
-            return transitions[t];
-        }
+  for (t in transitions) {
+    if (
+      document
+      .body
+      .contains(
+        _container.querySelectorAll(`#notify`)[0]
+      ) &&
+      _container.querySelectorAll(`#notify`)[0].style[t] !== undefined ||
+      _container.querySelectorAll(`.notifyEnd`)[0].style[t] !== undefined
+    ) {
+      return transitions[t];
     }
+  }
+  /* Listen for a transition! */
 }
-
-/* Listen for a transition! */
-var transitionEvent = whichTransitionEvent();
-transitionEvent && _notify.addEventListener(transitionEvent, function() {
-  _notify.classList.remove(`notify`);
-  _notify.style.display = `none`;
-});
