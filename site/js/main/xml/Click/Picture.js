90e9bbf289da19a9ea113622fae167367709eeae
Joey Banks <acktic@gmail.com>
2021-11-14 10:05:11 -0800

    mobile safari test copy paste background source

--- site/js/main/xml/Click/Picture.js
+++ site/js/main/xml/Click/Picture.js
@@ -5,17 +5,36 @@ _container
         evt.target.classList.contains(`fa-camera`) ||
         evt.target.classList.contains(`picture`)
       ) {
-        evt
-          .target
-          .closest(
-            `.item`
-          )
-          .querySelector(
-            `.source`
-          )
-          .select();
-        document.execCommand(`copy`);
-      }
+        if (navigator.clipboard) {
+          var myText =
+          evt
+            .target
+            .closest(
+              `.item`
+            )
+            .querySelector(
+              `.source`
+            ).value
+          ;
+          navigator.clipboard.writeText(myText).then(function() {
+            notifyOption(`Copied`, `fa-check-circle`);
+          }).catch(function() {
+            notifyOption(`Failed`, `fa-times-circle`);
+            // Do something to indicate the copy failed
+          });
+        } else {
+          evt
+            .target
+            .closest(
+              `.item`
+            )
+            .querySelector(
+              `.source`
+            )
+            .select();
+          document.execCommand(`copy`);
+          // Here's where you put the fallback code for older browsers.
+        }      }
     }, {
       passive: false
     }
