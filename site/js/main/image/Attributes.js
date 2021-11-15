e9a1e1811af261a5ace3c6e1c19eed47ccf21c63
Joey Banks <acktic@gmail.com>
2021-11-15 15:08:33 -0800

    Attributes.js remove item height cssText, Mobile.css yt item min-height

--- site/js/main/image/Attributes.js
+++ site/js/main/image/Attributes.js
@@ -138,8 +138,7 @@ var Attributes = function(empty, menuObject, pubIndex, src) {
                             !cropImages
                           ) {
                             item.style
-                              .cssText =
-                              `height:${itemImage.clientHeight}px !important`
+                              .height = `${itemImage.clientHeight}px`
                           }
                         }, 1
                       )
@@ -196,8 +195,7 @@ var Attributes = function(empty, menuObject, pubIndex, src) {
                           !cropImages
                         ) {
                           item.style
-                            .cssText =
-                            `height:${itemImage.clientHeight}px !important`
+                            .height = `${itemImage.clientHeight}px`
                         }
                       }, 1
                     )
@@ -266,8 +264,7 @@ var Attributes = function(empty, menuObject, pubIndex, src) {
                   !cropImages
                 ) {
                   item.style
-                    .cssText =
-                    `height:${itemImage.clientHeight}px !important`
+                    .height = `${itemImage.clientHeight}px`
                 }
               }, 1
             )
@@ -291,34 +288,31 @@ var Attributes = function(empty, menuObject, pubIndex, src) {
       }; //END NEWIMG.ONLOAD
       if (
         cropImages &&
-        window.innerWidth >= 768 &&
         display == `duo`
       ) {
-        itemContainer.style.cssText = `height:340px !important`;
-        itemImage.style.cssText = `height:340px !important`;
-        item.style.cssText = `height:340px !important`;
-        itemWrap.style.cssText = `height:340px !important`;
-        itemClassic.style.cssText = `height:340px !important`;
+        itemContainer.style.height = `340px`;
+        itemImage.style.height = `340px`;
+        item.style.height = `340px`;
+        itemWrap.style.height = `340px`;
+        itemClassic.style.height = `340px`;
       } else if (
         cropImages &&
-        window.innerWidth >= 768 &&
         display == `flexBox`
       ) {
-        itemContainer.style.cssText = `height:169px !important`;
-        itemImage.style.cssText = `height:169px !important`;
-        item.style.cssText = `height:169px !important`;
-        itemWrap.style.cssText = `height:169px !important`;
-        itemClassic.style.cssText = `height:169px !important`;
+        itemContainer.style.height = `169px`;
+        itemImage.style.height = `169px`;
+        item.style.height = `169px`;
+        itemWrap.style.height = `169px`;
+        itemClassic.style.height = `169px`;
       } else if (
-        window.innerWidth < 768 &&
         cropImages
       ) {
-        itemContainer.style.cssText = `height:80px !important`;
-        itemPending.style.cssText = `height:80px !important`;
-        itemImage.style.cssText = `height:80px !important`;
-        item.style.cssText = `height:80px !important`;
-        itemWrap.style.cssText = `height:80px !important`;
-        itemClassic.style.cssText = `height:80px !important`;
+        itemContainer.style.height = `80px`;
+        itemPending.style.height = `80px`;
+        itemImage.style.height = `80px`;
+        item.style.height = `80px`;
+        itemWrap.style.height = `80px`;
+        itemClassic.style.height = `80px`;
       }
 
       if (
