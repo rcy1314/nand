66f8d10f8b702b8f956cbe88e4f31aac65983a92
Joey Banks <acktic@gmail.com>
2021-12-10 05:14:36 -0800

    Content.js click remove refresh, scrollTo timeout

--- site/js/main/xml/Append.js
+++ site/js/main/xml/Append.js
@@ -166,7 +166,6 @@ var Append = function(id) {
     Suggest();
     Status(id, recent, oldest, posts);
   } else {
-    asset = [];
     while (
       _status.firstChild
     )
--- site/js/main/xml/Events/Click/Content.js
+++ site/js/main/xml/Events/Click/Content.js
@@ -13,20 +13,6 @@ _container
           location.href.split(`?`)[0]
         )
           location.href.split(`?`)[0].state();
-
-        while (
-          _status.firstChild
-        )
-          _status.removeChild(
-            _status.lastChild
-          );
-
-        while (
-          _suggestions.firstChild
-        )
-          _suggestions.removeChild(
-            _suggestions.lastChild
-          );
         Request(
           evt
           .target
@@ -66,7 +52,7 @@ _container
                 _channel.querySelector(`[aria-object='${evt.target.closest(`.suggest`).getAttribute(`aria-object`)}']`),
                 250
               );
-          }, 1000
+          }, 1500
         )
       }
 
