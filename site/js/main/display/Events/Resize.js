585fe87a02349f9f652d08040f3144dc4838fb70
Joey Banks <acktic@gmail.com>
2021-12-20 12:47:44 -0800

    display/Resize.js height mobile/larger

--- site/js/main/display/Events/Resize.js
+++ site/js/main/display/Events/Resize.js
@@ -16,28 +16,45 @@ window
           display = `flexBox`;
           Flex();
           setTimeout(
-            () => {
+            function() {
               _channel
                 .querySelectorAll(
-                  `.item, .wrap, .image, .classic`
+                  `.item, .img, .wrap, .image, .classic`
+                )
+                .forEach(
+                  function(a) {
+                    a
+                      .style
+                      .cssText =
+                      `height:80px`
+                  }
+                )
+            }, 25
+          )
+        } else {
+          setTimeout(
+            function() {
+              _channel
+                .querySelectorAll(
+                  `.item, .img, .wrap, .image, .classic`
                 )
                 .forEach(
                   function(a) {
                     if (a.closest(`.item`).querySelector(`.img`).clientHeight > 0)
                       a
                       .style
-                      .height =
-                      `${
-                              a
-                                .closest(
-                                  `.item`
-                                ).querySelector(
-                                  `.img`
-                                ).clientHeight
-                            }px`
+                      .cssText =
+                      `height:${
+                          a
+                            .closest(
+                              `.item`
+                            ).querySelector(
+                              `.img`
+                            ).clientHeight
+                        }px`
                   }
                 )
-            }, 1
+            }, 25
           )
         }
       }
