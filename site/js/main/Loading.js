30ff966a6ae841d9539ce1388b4ecf2cd662ed14
Joey Banks <acktic@gmail.com>
2022-01-11 11:37:49 -0800

    Update Loading.js

--- site/js/main/Loading.js
+++ site/js/main/Loading.js
@@ -81,5 +81,9 @@ setTimeout(
       Blocks = true;
       List = false;
     }
+    notifyOption(`${settings.length} Settings`, `fa-check-circle`);
+    notifyOption(`${exclude.length} Filters`, `fa-check-circle`);
+    notifyOption(`${view.length} Views`, `fa-check-circle`);
+    notifyOption(`${themes.length} Themes`, `fa-check-circle`);
   }, 500
 )
