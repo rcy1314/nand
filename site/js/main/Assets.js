83f8866535c370f0d9168f44b5f6b7926d6b6555
Joey Banks <acktic@gmail.com>
2022-01-13 13:31:46 -0800

    Update Assets.js

--- site/js/main/Assets.js
+++ site/js/main/Assets.js
@@ -29,14 +29,16 @@
        userName: https://www.youtube.com/feeds/videos.xml?user=
        channel: https://www.youtube.com/feeds/videos.xml?channel_id=
 
+       *** Example
        {id:`Test`,category:`Reddit`,title:``,description:`Reddit`,uri:`https//`,image:`Reddit`,hash:`QW`,media:!0},
+
        */
 
-menu.push(
+menu.unshift(
 
-  {id:`4Chan/SP`,category:`Reddit`,title:``,description:`4Chan Sports`,uri:`https://boards.4channel.org/sp/index.rss`,image:`4chan`,hash:`4S`,media:!0},
-  {id:`4Chan/Gif`,category:`Reddit`,title:``,description:`4Chan Gif`,uri:`https://boards.4channel.org/gif/index.rss`,image:`4chan`,hash:`4G`,media:!1},
-  {id:`4Chan/HR`,category:`Reddit`,title:``,description:`4Chan High Resolution`,uri:`https://boards.4channel.org/hr/index.rss`,image:`4chan`,hash:`4H`,media:!0},
-  {id:`4Chan/G`,category:`Reddit`,title:``,description:`4Chan Technology`,uri:`https://boards.4channel.org/g/index.rss`,image:`4chan`,hash:`4T`,media:!0},
+  {id:`4Chan/Sports`,category:`Reddit`,title:``,description:`4Chan Sports`,uri:`https://boards.4channel.org/sp/index.rss`,image:`4chan`,hash:`4S`,media:!0},
+  {id:`4Chan/Gifs`,category:`Reddit`,title:``,description:`4Chan Gif`,uri:`https://boards.4channel.org/gif/index.rss`,image:`4chan`,hash:`4G`,media:!1},
+  {id:`4Chan/High Resolution`,category:`Reddit`,title:``,description:`4Chan High Resolution`,uri:`https://boards.4channel.org/hr/index.rss`,image:`4chan`,hash:`4H`,media:!0},
+  {id:`4Chan/Technology`,category:`Reddit`,title:``,description:`4Chan Technology`,uri:`https://boards.4channel.org/g/index.rss`,image:`4chan`,hash:`4T`,media:!0},
 
 );
