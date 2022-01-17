a0ef0344af2b65d1a6d76a6cef9b57d29885f679
Joey Banks <acktic@gmail.com>
2022-01-17 12:22:06 -0800

    Update Scrollleft.js

--- site/js/main/xml/Events/Scrollleft.js
+++ site/js/main/xml/Events/Scrollleft.js
@@ -23,7 +23,6 @@ _channel
               _channel.scrollLeft -
               _channel.clientWidth <=
               _channel.clientWidth &&
-              touchmove == true &&
               Reader &&
               !stop &&
               _channel.querySelectorAll(`.pending`).length <= 3
