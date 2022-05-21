c132a445de1ee2288d080ec66bf9db7196f069de
ack <acktic@gmail.com>
2022-05-21 16:25:54 -0700

    Create Pretty.js

---
+++ site/js/main/Pretty.js
@@ -0,0 +1,74 @@
+let Pretty = function() {
+    let httpRequest;
+    let pub = [];
+    httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+
+        if (
+            httpRequest.readyState == 4
+        ) {
+            // 4 = `loaded`
+            if (
+                httpRequest.status == 200 &&
+                this.responseXML
+            ) {
+                console.log(`ok`)
+                    // 200 = OK
+                let xhr = this.responseXML;
+                if (
+                    xhr
+                    .getElementsByTagName(
+                        `entry`
+                    )
+                    .length >
+                    0
+                )
+                    var channel = `entry`;
+
+                else
+                    var channel = `item`;
+                for (
+                    let i = 0; i <= xhr.getElementsByTagName(channel).length - 1; i++
+                ) {
+                    let data =
+                        xhr
+                        .getElementsByTagName(
+                            `entry`
+                        )[i];
+                    let src = Source(data);
+
+                    pub.push({
+                        src: src,
+                    });
+                }
+                console.log(pub);
+                if (
+                    Array.isArray(pub)
+                )
+                    for (i = 0; i < pub.length - 1; i++) {
+                        console.log(i);
+                        if (!pub[i].src.match(/external/g)) {
+                            console.log(pub[i].src)
+                            let newImg;
+                            newImg = new Image();
+                            newImg.setAttribute(`src`, pub[i].src);
+                            newImg.onload = function() {
+                                if (
+                                    newImg.naturalWidth > 3400
+                                ) {
+                                    _container.style.backgroundImage = `url(${pub[i].src})`;
+                                    console.log(newImg.naturalWidth)
+                                }
+                            }
+                        }
+                    }
+            }
+        }
+    }
+    httpRequest.open(`GET`, `https://acktic-github-io.herokuapp.com/https://reddit.com/r/earthporn/.rss`);
+    httpRequest.setRequestHeader(`Content-Type`, `text/html; charset=utf-8`);
+    httpRequest.setRequestHeader(`Accept`, `text/html; charset=utf-8`);
+    httpRequest.setRequestHeader(`Access-Control-Allow-Origin`, `*`);
+    httpRequest.setRequestHeader(`X-Requested-With`, `*`);
+    httpRequest.send();
+}
